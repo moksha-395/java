df0771aa8ab9a476e2898c683936cc740087dce4
Moksha <mokshagna395@gmail.com>
2025-12-27 07:07:52 +0530

    Fix class declaration and output message in Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-class Test{
-  Public static void main(String args[]){
-    System.out.println("Hello World");
-      }
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
